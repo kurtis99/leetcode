9028e8b11e14b803c627d613e51c4abca1f2d631
Misha Boiko <mikhailboiko85@gmail.com>
2025-01-07 00:51:44 +0100

    Solution for 26. Remove Duplicates from Sorted Array

---
+++ 26_Remove_Duplicates_from_Sorted_Array/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+
+
+/*
+1)  1 1 1 1 2 2 3 3 3 3
+last_seen_val = 1
+last_seen_idx = 0
+*/
+
+
+
+
+int removeDuplicates(int* nums, int numsSize) {
+    int last_elem = nums[0];
+    int copy_index = 1;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == last_elem) {
+            continue;
+        } else {
+            last_elem = nums[i];
+        }
+        nums[copy_index++] = nums[i];
+    }
+
+    return copy_index; 
+}
+
+/* 
+Example 1:
+    Input: nums = [1,1,1,2,2,3]
+    Output: 5, nums = [1,1,2,2,3,_]
+    Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+    It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+    Input: nums = [0,0,1,1,1,1,2,3,3]
+    Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+    Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+    It does not matter what you leave beyond the returned k (hence they are underscores). */
+
+int main() {
+    int nums[] = {1,1,2};
+    int k;
+
+    k = removeDuplicates(nums, sizeof(nums) / sizeof(nums[0]));
+    printf("%d\n", k);
+    int nums2[] = {0,0,1,1,1,2,2,3,3,4};
+    k = removeDuplicates(nums2, sizeof(nums2) / sizeof(nums2[0]));
+    printf("%d\n", k);
+
+    return 0;
+}
